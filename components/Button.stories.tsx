dcec8fd7a7ec1f972905b81106ad38a8ee40b9ee
Patrik Niebur <patrik@cogo.co>
2022-04-18 11:39:46 +0100

    add storybook

---
+++ components/Button.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+export default {
+  title: 'React Native Button',
+  component: Button,
+  args: {
+    title: 'Hello worlds',
+  },
+};
+
+export const Basic = args => <Button {...args} />;
+export const Added = () => <Button title="test" />;
